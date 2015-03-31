2eabe25884282a4296b417a576e9091cab5f4359
supercygnus <yueyiqiu100@gmail.com>
2015-03-31 15:23:44 +0800

    201503311523

--- BookManagement/src/main/java/servlet/ManageServlet.java
+++ BookManagement/src/main/java/servlet/ManageServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ManageServlet
  */
-@WebServlet("/ManageServlet")
+
 public class ManageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
